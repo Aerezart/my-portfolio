ac3f22ff69ffb807efb537207ca6aafb7be08836
Aerezart <aerezart@gmail.com>
2023-04-06 20:32:03 +0200

    Update

--- src/pages/sections/Projects.js
+++ src/pages/sections/Projects.js
@@ -8,26 +8,16 @@ const Projects = () => {
     <Card>
       <Title functionLeft={"::("} functionRight={")"}>Projects - React</Title>
       <div className={`${classes.projects_flex} ${classes.full_width}`}>
+        <a href="https://aerezart.github.io/treasure-hunt-game/" target="_blank" rel="noreferrer"  className={`${classes.project}`}>
+          <p>https://</p>
+          <div>
+            <p>treasure-hunt-game</p>
+          </div>
+        </a>
         <a href="https://adamietz.pl/" target="_blank" rel="noreferrer"  className={`${classes.project} ${classes.disabled}`}>
           <p>https://</p>
           <div>
             <p>quizyfy.com (in development)</p>
-          </div>
-        </a>
-      </div>
-      <Spacer />
-      <Title functionLeft={"::("} functionRight={")"}>Projects - Wordpress</Title>
-      <div className={classes.projects_flex}>
-        <a href="https://adamietz.pl/" target="_blank" rel="noreferrer"  className={classes.project}>
-          <p>https://</p>
-          <div>
-            <p>adamietz.pl</p>
-          </div>
-        </a>
-        <a href="https://eurotronic.pl/" target="_blank" rel="noreferrer"  className={classes.project}>
-          <p>https://</p>
-          <div>
-            <p>eurotronic.pl</p>
           </div>
         </a>
       </div>
